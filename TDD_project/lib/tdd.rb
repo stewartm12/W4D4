427353e8dd7b22dc895794da0ac883f78a4b1d41
Lane Harris <lrharris215@gmail.com>
2021-01-21 14:38:50 -0500

    Add Array#two_sum and my_transpose specs

--- TDD_project/lib/tdd.rb
+++ TDD_project/lib/tdd.rb
@@ -8,3 +8,28 @@ def remove_dups(array)
 
   new_arr 
 end
+
+class Array
+  def two_sum
+    new_arr = []
+    self.each_with_index do |ele1, i1|
+      self.each_with_index do |ele2, i2|
+        if i2 > i1 && ele1 + ele2 == 0
+          new_arr << [i1, i2]
+        end
+      end
+    end
+    return new_arr
+  end
+
+end
+
+def my_transpose(arr)
+  new_arr = Array.new(arr.length){Array.new(arr[0].length)}
+  arr.each_with_index do |sub_arr, i|
+    sub_arr.each_with_index do |ele, j|
+      new_arr[j][i] = ele
+    end
+  end
+  new_arr
+end
--- TDD_project/spec/tdd_spec.rb
+++ TDD_project/spec/tdd_spec.rb
@@ -7,6 +7,24 @@ describe "remove_dups" do
   end
 end
 
-describe "two_sum" do 
-  
+describe Array do
+  subject(:arr){Array.new([-1, 0, 2, -2, 1])}
+
+  describe "two_sum" do 
+    it "returns all pairs of positions where elements sum to 0" do
+      expect(arr.two_sum).to eq([[0,4],[2,3]])
+    end
+
+  end
+end
+
+describe "my_transpose" do 
+  subject(:arr){[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ]}
+  it "will convert the rows to columns in a 2D array" do 
+      expect(my_transpose(arr)).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
+  end
 end
