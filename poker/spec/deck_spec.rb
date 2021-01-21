1392a76b16f24baed4a2eaae7aad27643e3dc0f0
stewart morales <stewartmorales129@gmail.com>
2021-01-21 17:06:36 -0500

    Create specs for Deck#initialize

--- poker/spec/deck_spec.rb
+++ poker/spec/deck_spec.rb
@@ -1,1 +1,18 @@
-require
+require "deck"
+require 'rspec'
+
+describe Deck do
+subject(:deck) {Deck.new}
+
+  describe "#initialize" do 
+    it "makes 52 card.instances" do 
+      expect(deck.cards.length).to eq(52)
+    end
+
+    it "contains thirteen of each suit" do 
+      
+    end
+  end
+
+  
+end
