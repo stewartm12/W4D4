1992e813e97c4ea5b0d5a73e24b0247f7c6a6ae7
stewart morales <stewartmorales129@gmail.com>
2021-01-21 16:12:24 -0500

    add #user_input and #print

--- TDD_project/lib/hanoi.rb
+++ TDD_project/lib/hanoi.rb
@@ -10,10 +10,35 @@ class TowerHanoi
   def move(tower1, tower2)
     if tower2.empty? || tower2.last > tower1.last
         disc = tower1.pop
-
         tower2 << disc
     else
      raise "Cannot move bigger disc on top of a smaller disc" if tower2.last < tower1.last
+    end
+  end
+
+  def win 
+    tower3 == [5, 4, 3, 2, 1]
+  end
+
+  def print
+    p tower1
+    p tower2
+    p tower3
+  end
+
+  def user_input
+    puts "Move piece in the form as '1 2'"
+    user_in = gets.chomp.split
+    user_in.map! do |num|
+      return tower1 if num == "1"
+      return tower2 if num == "2"
+      return tower3 if num == "3"
+    end
+  end
+
+  def play
+    while !win
+      
     end
   end
 
